9b3f44dc48bab4e3346b7788f0a10961385baf61
mompei <ogunborjey@gmail.com>
2025-06-30 01:18:38 +0100

    update

--- src/main.rs
+++ src/main.rs
@@ -33,9 +33,7 @@ async fn main() {
         .layer(cors);
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
-        .await
-        .unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
 
     axum::serve(listener, app).await.unwrap();
 }
